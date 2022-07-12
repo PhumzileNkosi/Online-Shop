f7b32a07b8f9e23feb0e0b39fd61f061d986667c
PauloN <PauloN@bbd.co.za>
2022-07-12 22:31:10 +0200

    escape user input on a query

--- src/helpers/connect.js
+++ src/helpers/connect.js
@@ -40,7 +40,7 @@ let getProductById = function(id) {
 let getProductsByName = function(name) {
     return new Promise(function(resolve, reject){
         connection.query(
-            "SELECT BIN_TO_UUID(ProductID) AS Product_ID, Name,Description, Price, Quantity FROM Product where Name='"+name +"';", 
+            "SELECT BIN_TO_UUID(ProductID) AS Product_ID, Name,Description, Price, Quantity FROM Product where Name='"+connection.escape(name) +"';", 
             function(err, rows){                                                
                 if(rows === undefined){
                     reject(err.message + "At connect.js line 40 func 'getProductsByName'");
