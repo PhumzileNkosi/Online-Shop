8173cd1eca3b06fca2605ad25b2e31582dbb9dc1
Carmen Janse van Rensburg <carmen@bbd.co.za>
2022-07-13 08:59:48 +0200

    updated helmet policy for aws

--- Server.js
+++ Server.js
@@ -33,7 +33,8 @@ app.use(helmet({
     directives: {
       "img-src": ["'self'",
                   "https://s.gravatar.com",
-                  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"]
+                  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+                  "https://onlinestore.bbdgp.cloud"]
     }
   },
   crossOriginEmbedderPolicy: false
